4b46484397547cc6067926a5fe5dcd0f8792597b
Manan Gupta <manangupta27.mg@gmail.com>
2025-03-03 23:40:30 +0530

    [Manan] ADD:Displaying the frequency of digits in a number

---
+++ FrequencyOfDigits.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FrequencyOfDigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+
+        // Array to store frequency of each digit (0-9)
+        int[] frequency = new int[10];
+
+        // Count frequency of each digit
+        while (num > 0) {
+            int digit = num % 10;
+            frequency[digit]++;
+            num /= 10;
+        }
+
+        // Display digit frequencies
+        System.out.println("Digit | Frequency");
+        for (int i = 0; i < 10; i++) {
+            if (frequency[i] > 0) {
+                System.out.println(i + " | " + frequency[i]);
+            }
+        }
+    }
+}
